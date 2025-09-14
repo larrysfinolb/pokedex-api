75b319200b64b024381b0ade69f363c1cbdb11d0
Larry Finol <larrysfinolb@gmail.com>
2025-09-14 09:45:27 -0400

    refactor: implement AxiosAdapter for HTTP requests and integrate into SeedService and CommonModule

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,20 +4,20 @@ import { PokeapiResponse } from './interfaces/pokeapi-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await this.axios.get<PokeapiResponse>(
+    const data = await this.http.get<PokeapiResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 

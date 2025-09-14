c26677ddbe1e5df40f1ee297fce9d20dfb38e3b9
Larry Finol <larrysfinolb@gmail.com>
2025-09-14 08:59:54 -0400

    feat: add SeedModule, SeedController, and SeedService for database seeding functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/pokedex'),
     PokemonModule,
     CommonModule,
+    SeedModule,
   ],
   controllers: [],
   providers: [],

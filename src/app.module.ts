cb2f858a2acadfd9aeeac8fea39a6c44cdf209d8
Larry Finol <larrysfinolb@gmail.com>
2025-09-14 14:44:28 -0400

    feat: add configuration management with Joi validation and environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
     MongooseModule.forRoot('mongodb://localhost:27017/pokedex'),
     PokemonModule,
     CommonModule,
